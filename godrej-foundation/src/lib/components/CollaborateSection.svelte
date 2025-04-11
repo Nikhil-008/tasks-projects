41ebca6536e7496143a68d80d68a1084cfd20334
nikhil <nikhil@able.do>
2025-04-11 14:50:44 +0530

    remove unwanted

--- godrej-foundation/src/lib/components/CollaborateSection.svelte
+++
@@ -1,7 +0,0 @@
-<section class="mx-auto max-w-lg px-4 py-8 text-center">
-	<p class="spectral-extralight text-2xl font-medium md:text-4xl">Collaborate With Us</p>
-	<p class="my-4 text-sm capitalize md:my-8 md:text-lg">
-		if you have a project, idea or area of focus you'd like to work on with us, please reach out
-	</p>
-	<a href="/contact" class="contact-button"> Contact Us </a>
-</section>
